31a6243a73180a0da83cf379897d0af2600396d2
leaveshine <qulixing888@163.com>
2017-02-21 18:20:31 +0800

    css

---
+++ js/variable.js
@@ -0,0 +1,5 @@
+var popfriend=document.getElementsByClassName("friendOn");
+var timer;
+var navct=document.getElementsByClassName("navct");
+var showcontent=document.getElementsByClassName("showcontent");
+var leftnavs=document.getElementsByClassName("lnavc");
